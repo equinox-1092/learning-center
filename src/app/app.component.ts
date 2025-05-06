552d74c44cc235e53c754e247d8b65e9ad01cddd
equinox-1092 <sdiaz1234di@gmail.com>
2025-05-06 11:33:04 -0500

    feat(main): add maain view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar,MatToolbarRow, LanguageSwitcherComponent, RouterLink, RouterLinkActive, TranslatePipe, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learning-center';
+  options = [
+    {link:'/home',label:'home'},
+    {link:'/about',label:'about'}
+  ]
+
+  constructor(private translate:TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
